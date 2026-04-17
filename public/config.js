cdc116f1f66501d409d04e1d1cb78d606a0d2b7c
Miłosz Cukla <73020087+Petitts@users.noreply.github.com>
2026-04-17 17:31:32 +0200

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,14 @@
+// Konfiguracja aplikacji frontendowej
+const APP_CONFIG = {
+    apiEndpoint: "/api",
+    // Klucz publiczny do weryfikacji JWT po stronie klienta
+    serverPublicKey: `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0vzc4sZBpNJo92YCIC32
+cBqyJw+k1Aa2FdCq+3GyLnwUpKddSpDAA2pIj9PHMqLEEkOcWbNlin0ANVucXq31
+5gi/2flzqvn7fPJ6CF1JiN8lOhCzFsfJb8PTFb17Yswigzy/moqpnS2A4SABU2kn
+5QHCLlbPXVBhK/O7XkLU7nwRjVu0B7AUGuJvsU+jkaqKcr6I5res+Rr5TOK4GCmB
+YWsQMJsgxnTEOU5d5kwWMIoX0laaDDrDFxR43ws2zXJkhRQ3ABEK8vhhUvuczX3t
+5kEkCjuO3m2qbUOKDQm10q6yTp9HvI+yhM1TDfA693s71H4sH+/7vvlZXXBQWN6o
+RQIDAQAB
+-----END PUBLIC KEY-----`
+};
