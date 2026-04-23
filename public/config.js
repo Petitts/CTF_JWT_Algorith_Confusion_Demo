b6239190c9f98e70ab39107708746eeb889197fa
Miłosz Cukla <73020087+Petitts@users.noreply.github.com>
2026-04-23 18:04:42 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -3,12 +3,12 @@ const APP_CONFIG = {
     apiEndpoint: "/api",
     // Klucz publiczny do weryfikacji JWT po stronie klienta
     serverPublicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0vzc4sZBpNJo92YCIC32
-cBqyJw+k1Aa2FdCq+3GyLnwUpKddSpDAA2pIj9PHMqLEEkOcWbNlin0ANVucXq31
-5gi/2flzqvn7fPJ6CF1JiN8lOhCzFsfJb8PTFb17Yswigzy/moqpnS2A4SABU2kn
-5QHCLlbPXVBhK/O7XkLU7nwRjVu0B7AUGuJvsU+jkaqKcr6I5res+Rr5TOK4GCmB
-YWsQMJsgxnTEOU5d5kwWMIoX0laaDDrDFxR43ws2zXJkhRQ3ABEK8vhhUvuczX3t
-5kEkCjuO3m2qbUOKDQm10q6yTp9HvI+yhM1TDfA693s71H4sH+/7vvlZXXBQWN6o
-RQIDAQAB
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqxx7NgOBSjdQdsseIdZz                                                                                                                                                                            
+JKUw8tRl8cI7dua/620w3ES0lyKVLGiyolBBHdpiMQJr2q/KhkozwRTR24UmV3eM                                                                                                                                                                            
+zZwsNM/+P21wkzu6oEtcDZmfKd+W7Mosd3pXU4eftGhc79d7Bipmh4QQsFgKXZUk                                                                                                                                                                            
+CZ9Ne+fLo4vzx26PMbj8DZh+USQLmrBeWRAu+Oz+TBp5hrtYdRT+McDdmK1bnuUZ
+i58Cvsgm1TqNSvJLEvOnsSZhMVOP7kEBIfd5iUM2EuV9tKKnwW5u47N/oeE4ssrz
+PNgyL9TOmebjeQIMr3NDojABkiWuP0yscZGPhic4BHvSvb+KMo4lmtkVejtjNVWA
+bQIDAQAB
 -----END PUBLIC KEY-----`
 };
